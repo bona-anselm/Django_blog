b628532e7213a7cee28df8ce55557033de1313be
Bonaventure Anselm <bona.anselm@gmail.com>
2023-10-23 03:12:20 +0100

    Setting up templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'Bona Anselm',
+        'title': 'Blog Post 1',
+        'content': 'First Content',
+        'date_posted': '21-10-2023'
+    },
+    {
+        'author': 'Anselm',
+        'title': 'Blog Post 2',
+        'content': 'Second Content',
+        'date_posted': '20-10-2023'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
