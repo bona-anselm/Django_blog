aa8b0b6172e672a0e2bf46b7e3d9e6aa7754463d
Bonaventure Anselm <bona.anselm@gmail.com>
2023-11-27 20:55:53 +0100

    Implemented CRUD on Posts

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -11,3 +12,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # This function gets an absolute url for a specific post to help the PostCreateView class redirects users
+    # to the post they created immediately after creating it
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
